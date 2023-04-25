55646f127fe1a322423e15f1794f93bd35c90757
chizidotdev <chiziwokoma@gmail.com>
2023-04-25 21:24:07 +0100

    refactor: wrap routes around chi router mux

--- apps/api/main.go
+++ apps/api/main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
@@ -25,32 +26,61 @@ func main() {
 
 	r := chi.NewRouter()
 
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
 	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
 
-	r.Get("/items", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "application/json")
-		jsonBytes, err := json.Marshal(items)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		_, _ = w.Write(jsonBytes)
+	r.Use(middleware.Timeout(60 * time.Second))
+
+	r.Route("/api/v1/items", func(r chi.Router) {
+		r.Get("/", getItems)
+		r.Post("/", createItem)
+
+		r.Route("/{id}", func(r chi.Router) {
+			r.Get("/", getItemById)
+			// r.Put("/", updateItem)
+			// r.Delete("/", deleteItem)
+		})
 	})
 
-	r.Post("/items", func(w http.ResponseWriter, r *http.Request) {
-		var item Item
-		err := json.NewDecoder(r.Body).Decode(&item)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-			return
-		}
-
-		items = append(items, item)
-
-		w.WriteHeader(http.StatusCreated)
-		json.NewEncoder(w).Encode(item)
-	})
-
-	err := http.ListenAndServe(":8000", r)
+	err := http.ListenAndServe(":3333", r)
 	log.Fatal(err)
+}
+
+func getItems(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	jsonBytes, err := json.Marshal(items)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	_, _ = w.Write(jsonBytes)
+}
+
+func createItem(w http.ResponseWriter, r *http.Request) {
+	var item Item
+	err := json.NewDecoder(r.Body).Decode(&item)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	items = append(items, item)
+
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(item)
+}
+
+func getItemById(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+
+	for _, item := range items {
+		if id == string(item.ID) {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+
+	w.WriteHeader(http.StatusNotFound)
 }
