2dd1424a59954328a7657248ee40acf9c5b20d9d
chizidotdev <chiziwokoma@gmail.com>
2023-04-28 01:55:40 +0100

    feat: update models to gorm.Model defaults

--- apps/api/pkg/models/models.go
+++ apps/api/pkg/models/models.go
@@ -4,15 +4,14 @@ import "gorm.io/gorm"
 
 type Item struct {
 	gorm.Model
-	ID           string  `json:"id" gorm:"primaryKey"`
 	Name         string  `json:"name"`
 	BuyingPrice  float64 `json:"buying_price"`
 	SellingPrice float64 `json:"selling_price"`
+	Quantity     int     `json:"quantity"`
 }
 
 type Sale struct {
 	gorm.Model
-	ID        string  `json:"id" gorm:"primaryKey"`
 	ItemID    string  `json:"item_id"`
 	Quantity  int     `json:"quantity"`
 	Total     float64 `json:"total"`
