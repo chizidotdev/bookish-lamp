62fff067dd5db562fd669c44943890e5d37f08c0
chizidotdev <chiziwokoma@gmail.com>
2023-04-28 18:41:03 +0100

    refactor: centralize app.tsx and route configs

--- apps/client/src/app/app.tsx
+++ apps/client/src/app/app.tsx
@@ -1,54 +1,16 @@
-import { Route, Routes, Link } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from 'react-query';
-import { Items } from './items';
+import { RoutesConfig } from './routes';
+import { BrowserRouter } from 'react-router-dom';
 
 const queryClient = new QueryClient();
 
 function App() {
     return (
         <QueryClientProvider client={queryClient}>
-            <RoutesConfig />
+            <BrowserRouter>
+                <RoutesConfig />
+            </BrowserRouter>
         </QueryClientProvider>
-    );
-}
-
-export function RoutesConfig() {
-    return (
-        <>
-            <div role="navigation">
-                <ul>
-                    <li>
-                        <Link to="/">Home</Link>
-                    </li>
-                    <li>
-                        <Link to="/page-2">Page 2</Link>
-                    </li>
-                </ul>
-            </div>
-            <Routes>
-                <Route
-                    path="/"
-                    element={
-                        <div>
-                            This is the generated root route.{' '}
-                            <Link to="/page-2">Click here for page 2.</Link>
-                        </div>
-                    }
-                />
-                <Route
-                    path="/page-2"
-                    element={
-                        <div>
-                            <Link to="/">
-                                Click here to go back to root page.
-                            </Link>
-
-                            <Items />
-                        </div>
-                    }
-                />
-            </Routes>
-        </>
     );
 }
 
--- apps/client/src/app/items.tsx
+++ apps/client/src/app/items.tsx
@@ -28,17 +28,23 @@ export const Items = () => {
                     <Table>
                         <Table.Thead>
                             <Table.Row>
+                                <Table.Th>#</Table.Th>
                                 <Table.Th>Name</Table.Th>
+                                <Table.Th>Quantity</Table.Th>
                                 <Table.Th>Buying Price</Table.Th>
                                 <Table.Th>Selling Price</Table.Th>
+                                <Table.Th>Created At</Table.Th>
                             </Table.Row>
                         </Table.Thead>
                         <Table.Tbody>
-                            {data.map((item) => (
+                            {data.map((item, index) => (
                                 <Table.Row key={item.id}>
+                                    <Table.Td>{index}</Table.Td>
                                     <Table.Td>{item.name}</Table.Td>
+                                    <Table.Td>{item.quantity}</Table.Td>
                                     <Table.Td>{item.buying_price}</Table.Td>
                                     <Table.Td>{item.selling_price}</Table.Td>
+                                    <Table.Td>{new Date(item.CreatedAt).toLocaleString()}</Table.Td>
                                 </Table.Row>
                             ))}
                         </Table.Tbody>
@@ -54,6 +60,8 @@ type Item = {
     name: string;
     buying_price: number;
     selling_price: number;
+    quantity: number;
+    CreatedAt: string;
 };
 
 const getItems = async (): Promise<Item[]> => {
