b6167a3762be8400e8868e30c4e3c47ee5749834
chizidotdev <chiziwokoma@gmail.com>
2023-04-29 15:00:31 +0100

    feat: add cva to button lib

--- libs/ui/src/lib/button/button.tsx
+++ libs/ui/src/lib/button/button.tsx
@@ -1,12 +1,27 @@
 import { ComponentProps } from 'react';
+import { cva, type VariantProps } from 'class-variance-authority';
 
-export interface ButtonProps
-    extends Omit<ComponentProps<'button'>, 'className'> {
-    children: React.ReactNode;
-}
+export type ButtonProps = VariantProps<typeof buttonStyles> &
+    Omit<ComponentProps<'button'>, 'className'> & {
+        children: React.ReactNode;
+    };
 
-export function Button({ children }: ButtonProps) {
-    return <button className="btn">{children}</button>;
+const buttonStyles = cva(['btn'], {
+    variants: {
+        loading: { true: 'loading' },
+        size: {
+            small: ['btn-sm'],
+        },
+    },
+    defaultVariants: {
+        loading: false,
+    },
+});
+
+export function Button({ children, loading, size }: ButtonProps) {
+    return (
+        <button className={buttonStyles({ loading, size })}>{children}</button>
+    );
 }
 
 export default Button;
