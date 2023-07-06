b3189bc9ed2835b8a52345f60590323b626007c8
chizidotdev <chiziwokoma@gmail.com>
2023-07-06 01:08:35 +0100

    fix: update cookie settings in logout handler

--- internal/app/user/logout.go
+++ internal/app/user/logout.go
@@ -1,12 +1,13 @@
 package user
 
 import (
+	"github.com/chizidotdev/copia/pkg/utils"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
 
 func (u *userService) Logout(ctx *gin.Context) {
-	ctx.SetSameSite(http.SameSiteLaxMode)
-	ctx.SetCookie("Authorization", "", -1, "", "", false, true)
+	ctx.SetSameSite(http.SameSiteNoneMode)
+	ctx.SetCookie("Authorization", "", -1, "", utils.EnvVars.ClientDomain, true, true)
 	ctx.JSON(http.StatusOK, "Logged out successfully")
 }
