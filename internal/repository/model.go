7dd565da154bd68b581e109b9c76b6555a63f26f
chizidotdev <chiziwokoma@gmail.com>
2023-07-25 09:31:53 +0100

    fix: set json field names on models

--- internal/repository/model.go
+++ internal/repository/model.go
@@ -9,10 +9,10 @@ import (
 
 // Base contains common columns for all tables.
 type Base struct {
-	ID        uuid.UUID      `gorm:"primaryKey;column:id;"`
-	CreatedAt time.Time      `gorm:"column:created_at;"`
-	UpdatedAt time.Time      `gorm:"column:updated_at;"`
-	DeletedAt gorm.DeletedAt `gorm:"index;column:deleted_at;"`
+	ID        uuid.UUID      `gorm:"primaryKey" json:"id"`
+	CreatedAt time.Time      `json:"created_at"`
+	UpdatedAt time.Time      `json:"updated_at"`
+	DeletedAt gorm.DeletedAt `gorm:"index" json:"deleted_at"`
 }
 
 // BeforeCreate will set a UUID rather than numeric ID.
@@ -32,22 +32,22 @@ func (base *Base) BeforeCreate(db *gorm.DB) error {
 
 type Item struct {
 	Base
-	Title        string  `gorm:"not null;column:title;"`
-	BuyingPrice  float32 `gorm:"not null;column:buying_price;"`
-	SellingPrice float32 `gorm:"not null;column:selling_price;"`
-	Quantity     int64   `gorm:"not null;column:quantity;"`
-	Sales        []Sale  `gorm:"foreignKey:ItemID;"`
-	UserEmail    string  `gorm:"not null;column:user_email;"`
+	Title        string  `gorm:"not null" json:"title"`
+	BuyingPrice  float32 `gorm:"not null" json:"buying_price"`
+	SellingPrice float32 `gorm:"not null" json:"selling_price"`
+	Quantity     int64   `gorm:"not null" json:"quantity"`
+	Sales        []Sale  `gorm:"foreignKey:ItemID" json:"sales"`
+	UserEmail    string  `gorm:"not null" json:"user_email"`
 }
 
 type Sale struct {
 	Base
-	QuantitySold int64     `gorm:"not null;column:quantity_sold;"`
-	SalePrice    float32   `gorm:"not null;column:sale_price;"`
-	SaleDate     time.Time `gorm:"not null;column:sale_date;"`
-	CustomerName string    `gorm:"not null;column:customer_name;"`
-	ItemID       uuid.UUID `gorm:"not null;column:item_id;"`
-	UserEmail    string    `gorm:"not null;column:user_email;"`
+	QuantitySold int64     `gorm:"not null" json:"quantity_sold"`
+	SalePrice    float32   `gorm:"not null" json:"sale_price"`
+	SaleDate     time.Time `gorm:"not null" json:"sale_date"`
+	CustomerName string    `gorm:"not null" json:"customer_name"`
+	ItemID       uuid.UUID `gorm:"not null" json:"item_id"`
+	UserEmail    string    `gorm:"not null" json:"user_email"`
 }
 
 //type Order struct {
